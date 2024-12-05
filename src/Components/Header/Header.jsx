1df5ee2b74f2051f0690dd308c12784f249d1663
rubinnn <lin.rubin2@gmail.com>
2024-12-05 13:45:40 +0800

    change info to a button and allow for the links to open on new tab

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,7 +10,13 @@ const Header = () => {
         Hi! I am <span>Lin Rubin</span>.
       </h1>
       <div className="header-action">
-        <div className="my-resume">My resume</div>
+        <a
+          href="https://drive.google.com/file/d/1HizB15Hbm04Bk_7sd71MO8HK-n6wyw-X/view?usp=drive_link"
+          target="_blank"
+          className="my-resume"
+        >
+          My resume
+        </a>
       </div>
     </div>
   );
--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -9,11 +9,16 @@ const Info = () => {
       </div>
       <div className="info-section">
         <div className="info">
-          <a href="https://github.com/Rububzz">Github</a>
+          <a href="https://github.com/Rububzz" target="_blank">
+            <button>Github</button>
+          </a>
         </div>
         <div className="info">
-          <a href="https://www.linkedin.com/in/lin-rubin-3a71322aa/">
-            LinkedIn
+          <a
+            href="https://www.linkedin.com/in/lin-rubin-3a71322aa/"
+            target="_blank"
+          >
+            <button>LinkedIn</button>
           </a>
         </div>
       </div>
